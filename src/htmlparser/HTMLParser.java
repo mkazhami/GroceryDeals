157314764311c569b28bdf2e6b1d67a588631696
Mikhail <mkazhami@uwaterloo.ca>
2015-07-08 10:59:57 -0400

    formatted name + pricing

--- src/htmlparser/HTMLParser.java
+++ src/htmlparser/HTMLParser.java
@@ -30,7 +30,7 @@ public class HTMLParser {
             String page = Integer.toString(pageCount);
             Connection.Response response = null;
             try {
-                System.out.println("Testing page " + page);
+                System.out.println("Testing page " + page + "\n\n");
                 response = Jsoup.connect("https://www.sobeys.com/en/flyer?&page=" + page)
                         .userAgent("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/535.21 (KHTML, like Gecko) Chrome/19.0.1042.0 Safari/535.21")
                         .timeout(10000)
@@ -43,8 +43,37 @@ public class HTMLParser {
                     Elements body = doc.getElementsByClass("card-inset");
                     boolean isBodyEmpty = true;
                     for ( Element e : body ) {
+                    	// only consider classes that contain the subclass 'price'
+                    	// other classes could be named 'card-inset' - only products will have a price
                         if (e.children().hasClass("price")) {
-                            System.out.println(e.html() + "\n\n");
+                        	Document eDoc = Jsoup.parse(e.html());
+                        	Element priceAmount = eDoc.select("div.price > div.price-amount").first();
+                        	Element productName = eDoc.select("h6.x-small-bottom").first();
+                        	Element productDesc = eDoc.select("p").first();
+                        	String price = Jsoup.parse(priceAmount.html()).text();
+                        	String name = Jsoup.parse(productName.html()).text();
+                        	String desc = Jsoup.parse(productDesc.html()).text();
+                        	String[] split = null;
+                        	if(!price.equals("")) {
+                        		split = price.split("/");
+                        	}
+                        	
+                        	if(price.contains("ea.")) {
+                        		System.out.println(name + " are $" + split[0].replace(" ",  ".") + " each.");
+                        	}
+                        	else if(price.contains("lb.")) {
+                        		System.out.println(name + " is $" + split[0].replace(" ", ".") + " per pound.");
+                        	}
+                        	else if(price.contains("g")) {
+                        		System.out.println(name + " is $" + split[0].replace(" ", ".") + " per " + split[1].replace("g", "") + " grams.");
+                        	}
+                        	else if(split != null){ // of the form '2/1 00' meaning 2 for $1.00
+                        		System.out.println(name + " is " + split[1].replace(" ", ".") + " for $" + split[0]);
+                        	}
+                        	else if(desc.contains("BUY") && desc.contains("EARN")){ // has a points promotion rather than sale
+                        		String promotion = desc.substring(desc.indexOf("BUY"), desc.length());
+                        		System.out.println(name + " has a promotion: " + promotion);
+                        	}
                             isBodyEmpty = false;
                         }
                     }
