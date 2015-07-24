ea85ab867ab3b23fe7b863d9a70de91d674fb829
Mikhail <mikhail.kazhamiaka@gmail.com>
2015-07-23 22:16:14 -0400

    finished accessibility mode for loblaws

--- parseLoblawsAccessibility.py
+++ parseLoblawsAccessibility.py
@@ -17,8 +17,8 @@ import re
 
 # opens phantom browser
 # phantomjs.exe is located in the root directory
-#driver = webdriver.PhantomJS(executable_path="/home/mikhail/Documents/htmlparse/phantomjs_linux")
-driver = webdriver.PhantomJS()
+driver = webdriver.PhantomJS(executable_path="/home/mikhail/Documents/htmlparse/phantomjs_linux")
+#driver = webdriver.PhantomJS()
 driver.set_window_size(1400,1000)
 #driver = webdriver.Firefox()
 
@@ -96,10 +96,10 @@ for url in cityURLs:
     for link in viewFlyerLinks:
         # open specific store's flyer
         driver.get(link)
-        time.sleep(10)
+        time.sleep(8)
         # go to accessibility view
         driver.find_element_by_xpath("//ul[@class='sort-options tab-layout tabs-3']//li//a[@class='accessible-view']").click()
-        time.sleep(5)
+        time.sleep(8)
         try:
             # get all html elements that have product info
             driver.switch_to_frame(driver.find_element_by_xpath("//iframe[@id='videoFrame']"))
@@ -116,9 +116,104 @@ for url in cityURLs:
                     for product in products:
                         product = product.find_elements_by_xpath(".//tbody/tr")
                         name = product[0].text.encode('utf-8', 'ignore')
+                        price = ""
+                        quantity = "1"
+                        weight = ""
+                        limit = ""
+                        each = ""
+                        additionalInfo = ""
                         productInfo = product[1].text.encode('utf-8', 'ignore').replace("\n", " ")
-                        print("name: " + name + "      info: " + productInfo)
+                        
+                        # remove 'SAVE $x' from the info, not needed
+                        findSave = re.search("[Ss][Aa][Vv][Ee]", productInfo)
+                        if findSave is not None:
+                            productInfo = productInfo[:findSave.start()].strip()
 
+                        # check for '$x dozen' - usually used for flowers
+                        findDozen = re.search("\$[0-9]+( )*DOZEN", productInfo.upper())
+                        if findDozen is not None:
+                            price = productInfo[findDozen.start():findDozen.end()].upper().replace("DOZEN", "").strip()
+                            quantity = "12"
+                            productInfo = productInfo.replace(productInfo[findDozen.start():findDozen.end()], "").strip()
+                        
+                        # get the 'less than x $y each' deals
+                        findLessThan = re.search("LESS( )*THAN( )*[0-9]+( )*\$[0-9]+(\.| )[0-9]+( )*EA(\.|CH)", productInfo.upper())
+                        if findLessThan is not None:
+                            split = productInfo[findLessThan.start():findLessThan.end()].split("$")
+                            each = "$" + split[1].replace("each", "").replace("ea.", "").strip().replace(" ", ".")
+                            productInfo = productInfo.replace(productInfo[findLessThan.start():findLessThan.end()], "").strip()
+
+                        # get '2/$x or $y each" deals
+                        findOr = re.search('[0-9]+/\$[0-9]+(\.[0-9]+)?( )*OR( )*((\$[0-9]+(\.| )?[0-9]*)|([0-9]+.*))( )*EA(\.|CH)', productInfo.upper()) # ghetto way of matching 'cent' unicode
+                        if findOr is not None:
+                            split = productInfo[findOr.start():findOr.end()].upper().split("OR")
+                            quantityPriceSplit = split[0].split("/")
+                            quantity = quantityPriceSplit[0].strip()
+                            price = quantityPriceSplit[1].strip()
+                            each = split[1].replace("EACH", "").replace("EA.", "").strip().replace(" ", ".")
+                            productInfo = productInfo.replace(productInfo[findOr.start():findOr.end()], "").strip()
+
+                        # get weight unit of product - prioritize lb, but if only kg then use that
+                        # and take it out of the product info
+                        findLb = re.search("\$[0-9]*\.[0-9]*( )*/?( )*lb( )*(/)?", productInfo)
+                        if findLb is not None:
+                            price = productInfo[findLb.start():findLb.end()].split("lb")[0].strip()
+                            weight = "lb"
+                            productInfo = productInfo.replace(productInfo[findLb.start():findLb.end()], "").strip()
+                        else:
+                            findKg = re.search("\$[0-9]*\.[0-9]*( )*/( )*kg( )*(/)?", productInfo)
+                            if findKg is not None:
+                                price = productInfo[findKg.start():findKg.end()].split("kg")[0].replace("/", "").strip()
+                                weight = "kg"
+                                productInfo = productInfo.replace(productInfo[findKg.start():findKg.end()], "").strip()
+                            else:
+                                findG = re.search("\$[0-9]+((\.| )[0-9]+)?( )*/[0-9]+( )*[Gg]", productInfo)
+                                if findG is not None and price == "":
+                                    split = productInfo[findG.start():findG.end()].split("/")
+                                    # sometimes there is a price /x grams and sometimes price is separate
+                                    price = split[0].strip().replace(" ", ".")
+                                    weight = split[1].replace(" ", "")
+                                    productInfo = productInfo.replace(productInfo[findG.start():findG.end()], "").strip()
+
+                        # get item limit if applicable and remove from product info
+                        findLimit = re.search("LIMIT( )*[0-9]+( )*AFTER LIMIT( )*\$[0-9]*(\.| )[0-9]*( )*(EA(\.|CH))?", productInfo.upper())
+                        if findLimit is not None:
+                            split = productInfo[findLimit.start():findLimit.end()].upper().split("AFTER LIMIT")
+                            limit = split[0][re.search("[0-9]+", split[0]).start():].strip()
+                            each = split[1].replace("EACH", "").replace("EA.", "").strip().replace(" ", ".")
+                            productInfo = productInfo.replace(productInfo[findLimit.start():findLimit.end()], "").strip()
+
+                        # get 'x/$y' deals
+                        findXForY = re.search("[0-9]+( )*/( )*\$[0-9]+((\.| )[0-9]+)?", productInfo)
+                        if findXForY is not None:
+                            split = productInfo[findXForY.start():findXForY.end()].split("/")
+                            quantity = split[0].strip()
+                            price = split[1].strip().replace(" ", ".")
+                            productInfo = productInfo.replace(productInfo[findXForY.start():findXForY.end()], "").strip()
+                        
+                        # lastly get the price, should be the only thing remaining with a price
+                        # 'kg' price might still be there so '$' must be there
+                        findPrice = re.search('\$[0-9]+((\.| )[0-9]*)?(( )*(ea(\.|ch)))?', productInfo)
+                        if findPrice is not None and price == "":
+                            price = productInfo[findPrice.start():findPrice.end()].replace("each", "").replace("ea.", "").strip().replace(" ", ".")
+                            productInfo = productInfo.replace(productInfo[findPrice.start():findPrice.end()], "").strip()
+
+                        # sometimes there are multiple products, make sure we have the 'each' price if it's left
+                        findEach = re.search('\$[0-9]+((\.| )[0-9]*)?( )*(ea(\.|ch))', productInfo)
+                        if findEach is not None and each == "":
+                            each = productInfo[findEach.start():findEach.end()].replace("each", "").replace("ea.", "").strip().replace(" ", ".")
+                            productInfo = productInfo.replace(productInfo[findEach.start():findEach.end()], "").strip()
+
+                        if weight == "":
+                            print("name: " + name + "      price: " + price + "     quantity: " + quantity + "   limit: " + limit + "   each: " + each + "   info: " + productInfo)
+                        else:
+                            print("name: " + name + "      price: " + price + "     weight: " + weight + "   limit: " + limit + "   each: " + each + "   info: " + productInfo)
+                        print("\n")
+            exit()
+
+
+
+            driver.switch_to_default_content()
         except Exception as e:
             # print exception message and break from loop since we've gone through all the pages
             print(str(e))
