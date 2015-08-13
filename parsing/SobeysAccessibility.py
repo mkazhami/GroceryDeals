92e6aeb162109c293cfba5b2b8a53faefc368fb6
mkazhami <mikhail.kazhamiaka@gmail.com>
2015-08-12 23:36:06 -0400

    added name + price gathering for sobeys

--- parsing/SobeysAccessibility.py
+++ parsing/SobeysAccessibility.py
@@ -15,7 +15,7 @@ class Sobeys(BaseParseClass):
     """
     
     store_list_links = { 
-                         "sobeys" : ""
+                         "sobeys" : "https://www.sobeys.com/en/store-locator/"
                        }
     
     store_rewards_programs = {
@@ -37,13 +37,8 @@ class Sobeys(BaseParseClass):
         #driver = webdriver.PhantomJS(executable_path="phantomjs.exe", service_log_path=os.path.devnull)
         driver = webdriver.Firefox()
         driver.set_window_size(1400,1000)
-        trueStoresListLink = "https://www.sobeys.com/en/stores"
-        driver.get("https://www.sobeys.com/en/stores")
+        driver.get(self.store_list_links[self.store_name])
         time.sleep(8)
-        if len(driver.page_source) < 100:
-            trueStoresListLink = "https://www.sobeys.com/en/store-locator/"
-            driver.get("https://www.sobeys.com/en/store-locator/")
-            time.sleep(8)
         
 
         zoomOut = driver.find_element_by_xpath(".//div[@title='Zoom out']")
@@ -68,26 +63,46 @@ class Sobeys(BaseParseClass):
 
         for link in saveMyStoreLinks:
             # go back to page with store preference links
-            driver.get(trueStoresListLink)
-            time.sleep(5)
+            #driver.get(self.store_list_links[self.store_name])
+            #time.sleep(8)
             # get zoom out object and zoom out
-            zoomOut = driver.find_element_by_xpath(".//div[@title='Zoom out']")
-            for i in range(10):
-                zoomOut.click()
-                time.sleep(0.5)
-            time.sleep(1)
+            #zoomOut = driver.find_element_by_xpath(".//div[@title='Zoom out']")
+            #for i in range(10):
+            #    zoomOut.click()
+            #    time.sleep(0.5)
+            #time.sleep(3)
+            #try:
+            #    # try to get original preference link and click it
+            #    #driver.find_element_by_xpath(".//a[@class='btn btn-default save-as-my-store' and @href='" + link + "']").click()
+            #except:
+            #    try:
+            #        # try it with http instead of https - for some reason it changes...
+            #        driver.find_element_by_xpath(".//a[@class='btn btn-default save-as-my-store' and @href'" + link.replace("https", "http") + "']").click()
+            #    except:
+            #        try:
+            #            # if previous link fails, try just the relative path instead of full link
+            #            # if this fails then exit, because something is wrong
+            #            driver.find_element_by_xpath(".//a[@class='btn btn-default save-as-my-store' and @href='" + link.split(".com")[1] + "']").click()
+            #        except:
+            #            raise Exception("Unable to press button to save store as preference: " + link)
+            driver.get(link)
+            time.sleep(5)
+
             try:
-                # try to get original preference link and click it
-                driver.find_element_by_xpath(".//a[@href='" + link + "']").click()
+                driver.find_element_by_xpath(".//a[@id='save-as-my-store']").click()
+                time.sleep(3)
             except:
-                # if previous link fails, try just the relative path instead of full link
-                driver.find_element_by_xpath(".//a[@href='" + link.split(".com")[1] + "']").click()
-                
+                # if store is already selected, the button wouldn't be there - check for that
+                myStore = driver.find_element_by_xpath(".//ul[@class='list-inline']")
+                if link.split("com")[1] in myStore.get_attribute("innerHTML").encode('ascii', 'ignore'):
+                    pass # don't do anything, store is already selected
+                else:
+                    raise Exception("Save my store button not found")
+
             name = driver.find_element_by_xpath(".//div[@class='combo-right']//div[@class='left']").text.encode('ascii', 'ignore').strip()
             # there are a couple of elements with identical structure
             storeInformationElements = driver.find_elements_by_xpath(".//div[@class='col-sm-3']")
             for element in storeInformationElements:
-                print("going through a store information element")
                 html = element.get_attribute("innerHTML")
                 if "Store Number" in html:
                     storeNumber = str(element.find_element_by_xpath(".//p").get_attribute("innerHTML").encode('ascii', 'ignore')).strip()
@@ -114,19 +129,54 @@ class Sobeys(BaseParseClass):
             driver.get("https://www.sobeys.com/en/flyer")
             time.sleep(8)
 
+            # switch frame
+            driver.switch_to_frame(driver.find_element_by_xpath("//iframe[@id='flipp-iframe']"))
+            time.sleep(1)
+
             # go to item view
-            labelElements = driver.find_elements_by_xpath(".//div[@class='grid-view-label']")
-            for labelElement in labelElements:
-                if "Item View" in labelElement.get_attribute("innerHTML"):
-                    labelElement.click()
-                    break
+            labelElement = driver.find_element_by_xpath(".//div[@class='grid-view-label']")
+            if "Item View" in labelElement.get_attribute("innerHTML"):
+                labelElement.click()
+            else:
+                raise Exception("No 'Item View' found")
             time.sleep(8)
             
             # get all product elements
+            tries = 0
             products = driver.find_elements_by_xpath(".//li[@class='item']")
+            while len(products) == 0 and tries <= 5:
+                print("number of products: " + str(len(products)))
+                driver.refresh()
+                time.sleep(8)
+                driver.switch_to_frame(driver.find_element_by_xpath("//iframe[@id='flipp-iframe']"))
+                time.sleep(1)
+                driver.find_element_by_xpath(".//div[@class='grid-view-label']").click()
+                time.sleep(8)
+                products = driver.find_elements_by_xpath(".//li[@class='item']")
+                tries += 1
+            print("number of products: " + str(len(products)))
             
             for product in products:
-                print(str(product.get_attribute("innerHTML")))
+                #print(str(product.get_attribute("innerHTML").encode('ascii', 'ignore')))
+                name = product.find_element_by_xpath(".//div[@class='item-name']").get_attribute("innerHTML").encode('utf-8', 'ignore')
+                fullPrice = product.find_element_by_xpath(".//div[@class='item-price']")
+                # sobeys divides the price into '$' 'x' '.' 'xx' '¢ (but usually hidden)' 'rest of text (i.e. OR $1.99 EACH)'
+                priceText = fullPrice.find_elements_by_xpath(".//span")
+                if len(priceText) >= 7:
+                    prePriceText = priceText[0].text.encode('utf-8', 'ignore')
+                    dollarSign = priceText[1].text.encode('utf-8', 'ignore')
+                    dollar = priceText[2].text.encode('utf-8', 'ignore')
+                    dot = priceText[3].text.encode('utf-8', 'ignore')
+                    cents = priceText[4].text.encode('utf-8', 'ignore')
+                    centSign = priceText[5].text.encode('utf-8', 'ignore')
+                    restOfText = fullPrice.find_element_by_xpath(".//span[@class='price-text']").text.encode('utf-8', 'ignore')
+                    #restOfText = priceText[6].text.encode('utf-8', 'ignore')
+                    print(name + "      " + prePriceText + " " + dollarSign + " " + dollar + " " + dot + " " + cents + " " + centSign + " " + restOfText)
+                else:
+                    for i in range(len(priceText)):
+                        print(priceText[i].text.encode('utf-8', 'ignore'))
+                #if "$" in dollarSign:
+                #    price = dollar + dot + cents
             
             
 
